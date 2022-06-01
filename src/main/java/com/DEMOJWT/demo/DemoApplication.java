40421b397db5d891b56c2f9af91bc41ef870ac59
Alvaro Mena <alvaro16a@gmail.com>
2022-06-01 17:51:22 -0500

    Se clono el programa,se agregaron las dependencias y se modifico el archivo properties para acceder a una base de datos SQL

--- src/main/java/com/DEMOJWT/demo/DemoApplication.java
+++ src/main/java/com/DEMOJWT/demo/DemoApplication.java
@@ -1,14 +1,19 @@
 package com.DEMOJWT.demo;
 
 import com.DEMOJWT.demo.security.JWTAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -29,6 +34,7 @@ public class DemoApplication {
 					.authorizeRequests()
 					.antMatchers(HttpMethod.POST, "/user").permitAll()
 					.anyRequest().authenticated();
+
 		}
 	}
 
